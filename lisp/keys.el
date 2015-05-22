781fcd07648e873bbff0954877627a11b4faa1a1
Geoff Wark <gvvark@gmail.com>
2015-05-21 23:01:45 -0400

    Add neotree

--- lisp/keys.el
+++ lisp/keys.el
@@ -4,3 +4,7 @@
 (global-set-key   (kbd "C-S-c C-S-c") 'mc/edit-lines)
 (global-unset-key (kbd "M-<down-mouse-1>"))
 (global-set-key   (kbd "M-<mouse-1>") 'mc/add-cursor-on-click)
+
+;; neotree
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
